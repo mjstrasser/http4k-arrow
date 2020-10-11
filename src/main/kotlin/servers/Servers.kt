30627324f26f9c9c286101afba3102e07d91e318
Michael Strasser <michael@michaelstrasser.com>
2020-10-11 16:15:32 +1000

    Moved package

--- src/main/kotlin/servers/Servers.kt
+++ src/main/kotlin/servers/Servers.kt
@@ -1,10 +1,7 @@
-package mjs
+package servers
 
-import mu.KotlinLogging
 import org.http4k.core.HttpHandler
 import org.http4k.core.Response
 import org.http4k.core.Status
-
-private val logger = KotlinLogging.logger {}
 
 val helloServer: HttpHandler = { request -> Response(Status.OK).body("Hello, ${request.query("name")}!") }
--- src/test/kotlin/servers/ServersTest.kt
+++ src/test/kotlin/servers/ServersTest.kt
@@ -1,4 +1,4 @@
-package mjs
+package servers
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
