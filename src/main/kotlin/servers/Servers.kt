70ceb0943cccd8b5b87979da91bbeeccac2134c4
Michael Strasser <michael@michaelstrasser.com>
2020-10-16 21:32:53 +1000

    Direct use of requestIdFilter.then

--- src/main/kotlin/servers/Servers.kt
+++ src/main/kotlin/servers/Servers.kt
@@ -12,22 +12,20 @@ val requestIdFilter: Filter = Filter { next ->
     { request -> next(request).header(REQUEST_ID_HEADER, request.header(REQUEST_ID_HEADER)) }
 }
 
-fun reqFilter(handler: HttpHandler) = requestIdFilter.then(handler)
-
-val helloServer: HttpHandler = reqFilter { request -> Response(Status.OK).body("Hello, ${request.query("name")}!") }
+val helloServer: HttpHandler = requestIdFilter.then { request -> Response(Status.OK).body("Hello, ${request.query("name")}!") }
 
 fun String?.oddOrNull(): Int? = this?.toIntOrNull()?.let { if (it % 2 != 0) it else null }
 
-val onlyOddServer: HttpHandler = reqFilter { request ->
+val onlyOddServer: HttpHandler = requestIdFilter.then { request ->
     request.query("number").oddOrNull()?.let {
         Response(Status.OK).body("$it")
     } ?: Response(Status.BAD_REQUEST).body("bad")
 }
 
-val slowServer: HttpHandler = reqFilter { request ->
+val slowServer: HttpHandler = requestIdFilter.then { request ->
     val delay = request.query("delay")?.toIntOrNull() ?: 0
     Thread.sleep(delay * 1000L)
     Response(Status.OK).body("Waited!!")
 }
 
-val failServer: HttpHandler = reqFilter { Response(Status.INTERNAL_SERVER_ERROR).body("error") }
+val failServer: HttpHandler = requestIdFilter.then { Response(Status.INTERNAL_SERVER_ERROR).body("error") }
