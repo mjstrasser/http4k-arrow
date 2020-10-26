ba908af6dd0d92b290153d297b232fef5154f3f4
Michael Strasser <mstrass@thoughtworks.com>
2020-10-26 18:02:22 +1000

    Correct tob for Lucy

--- src/test/kotlin/servers/SerializationTest.kt
+++ src/test/kotlin/servers/SerializationTest.kt
@@ -42,8 +42,8 @@ class SerializationTest : DescribeSpec({
                     """{"name":"Mike","bornWhen":"1961-01-04T09:04+10:00"}"""
         }
         it("deserialises JSON as expected") {
-            val lucyBornWhen = ZonedDateTime.of(1961, 1, 4, 9, 11, 0, 0, ZoneOffset.ofHours(10))
-            Json.decodeFromString<Person>("""{"name":"Lucy","bornWhen":"1961-01-04T09:11+10:00"}""") shouldBe
+            val lucyBornWhen = ZonedDateTime.of(1961, 1, 4, 9, 15, 0, 0, ZoneOffset.ofHours(10))
+            Json.decodeFromString<Person>("""{"name":"Lucy","bornWhen":"1961-01-04T09:15+10:00"}""") shouldBe
                     Person("Lucy", lucyBornWhen)
 
         }
